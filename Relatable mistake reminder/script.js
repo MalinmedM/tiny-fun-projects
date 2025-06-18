bbc36ddb58479e880959d8fe75c3f0d1ea55082c
Malin Håkansson <malin.hson@gmail.com>
2025-06-18 23:38:20 +0200

    Added js array

---
+++ Relatable mistake reminder/script.js
@@ -0,0 +1,11 @@
+const reminders = [
+    "Apparently I love doing things twice. Who knew?",
+    "And the prize for Most Relatable Mistake goes to… me.",
+    "Oops? Sure. But shame? Nah, she's busy.",
+    "I was just bored. Nothing like making life a bit more interesting.",
+    "Did I mess up again? Must be Monday. Or Tuesday. Or...",
+    "File not saved? Oopsidaisy. How lucky am I to get to do it ALL OVER AGAIN!",
+    "Classic me. At least I’m consistent!",
+    "We grow through what we mess up. Probably.",
+    "One small mistake for man, one giant LOL for mankind."
+    ]; 
